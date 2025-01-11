a0dc996910572cede0c2bf66709f9413070d1eb7
ZeiPara <kito20111011@outlook.jp>
2025-01-11 13:45:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -91,6 +91,7 @@ app.post('/login', async (req, res) => {
 });
 
 // サーバー起動
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000; // 環境変数 PORT を優先
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
